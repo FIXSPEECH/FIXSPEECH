25a0976ab7ecc0ca2b7dd2cecac6441c3ad70771
최다희 <chdh8987@naver.com>
2024-10-29 17:48:07 +0900

    [FE] feat:메인페이지 사용자 헤더

--- frontend/src/components/MainPage/History.tsx
+++ frontend/src/components/MainPage/History.tsx
@@ -1,4 +1,24 @@
+// import CalendarHeatmap from 'react-calendar-heatmap';
+
 function History(){
+
+    // const cal: CalendarHeatmap = new CalendarHeatmap();
+
+    // cal.init({
+    //     itemSelector: '#heatmap',
+    //     domain: 'month',
+    //     subDomain: 'x',
+    //     range: 3, // 3개월 범위
+    //     data: {
+    //       // 데이터 로드
+    //       '2024-10-01': 1,
+    //       '2024-10-02': 2,
+    //       '2024-10-03': 3,
+    //     },
+    //     // 추가 설정...
+    //   });
+    
+
     return(
         <div style={{color: 'white'}}> 잔디..</div>
     )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from 'tailwindcss'
+// import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -41,4 +43,10 @@ export default defineConfig({
       },
     }),
   ],
+
+  css: {
+    postcss:{
+      plugins: [tailwindcss],
+    },
+  },
 });
