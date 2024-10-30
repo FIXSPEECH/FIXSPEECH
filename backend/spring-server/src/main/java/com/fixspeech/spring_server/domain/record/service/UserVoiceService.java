36207c02d8d209bb3b2257739b0ac97800a80cdd
김태연 <tobysamsung@naver.com>
2024-10-30 14:00:42 +0900

    [BE] fix : delete all BE file

--- backend/spring-server/src/main/java/com/fixspeech/spring_server/domain/record/service/UserVoiceService.java
+++
@@ -1,7 +0,0 @@
-package com.fixspeech.spring_server.domain.record.service;
-
-import com.fixspeech.spring_server.domain.record.dto.UserVoiceRequestDto;
-
-public interface UserVoiceService {
-	void saveImage(UserVoiceRequestDto userVoiceRequestDto);
-}
