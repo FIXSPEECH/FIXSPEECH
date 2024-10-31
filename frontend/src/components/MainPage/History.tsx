67558230cbbc2f1795d687084b404e29f53b0ef4
최다희 <chdh8987@naver.com>
2024-10-31 10:40:15 +0900

    [FE] fix:스트릭 높이 수정

--- frontend/src/components/MainPage/History.tsx
+++ frontend/src/components/MainPage/History.tsx
@@ -1,29 +1,24 @@
 // @ts-ignore
-import CalendarHeatmap from 'react-calendar-heatmap'
-import 'react-calendar-heatmap/dist/styles.css'
-import '../../styles/MainPage/History.css'
-// @ts-ignore
-// import ReactTooltip from 'react-tooltip';
+import CalendarHeatmap from 'react-calendar-heatmap';
+import '../../styles/MainPage/History.css';
+import { useState, useEffect } from 'react';
 
 function History() {
-
     return (
-        <div style={{marginLeft:'3%'}}  className='w-1/2  mt-5 mb-10'>
-        <CalendarHeatmap
-            startDate={new Date(new Date().setMonth(new Date().getMonth() - 4))}
-            endDate={new Date(new Date().setMonth(new Date().getMonth() ))}
-            values={[
-                { date: '2024-08-01', count: 12 },
-                { date: '2024-09-22', count: 122 },
-                { date: '2024-10-30', count: 38 },
-                // ...and so on
-            ]}
-            onClick={(value:any )=> alert(`Clicked on value with count: ${value.count}`)}
-           className="custom-calendar-height"
+        <div style={{ marginLeft: '3%', marginRight:'3%' }} className='mt-5 mb-10'>
+            <CalendarHeatmap
+                startDate={new Date(new Date().setMonth(new Date().getMonth() - 4))}
+                endDate={new Date(new Date().setMonth(new Date().getMonth()))}
+                values={[
+                    { date: '2024-08-01', count: 1 },
+                    { date: '2024-09-22', count: 2 },
+                    { date: '2024-10-30', count: 4 },
+                    // ...and so on
+                ]}
+                onClick={(value: any) => alert(`Clicked on value with count: ${value.count}`)}
             />
         </div>
-        
-    )
+    );
 }
 
 export default History;
