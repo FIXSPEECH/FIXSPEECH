d7998a8b3941e94750fca4609af32deb82db8a3a
김봉상 <tph02361@gmail.com>
2024-10-31 17:04:57 +0900

    [FE] PublicRoute, ProtectedRoute 추가에 따라 코드 간소화

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,20 +1,8 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import useAuthStore from "../../store/authStore";
 import Buttons from "../../components/MainPage/Buttons";
 import UserInfo from "../../components/MainPage/UserInfo";
 import History from "../../components/MainPage/History";
 
 function MainPage() {
-  const navigate = useNavigate();
-  const isLogin = useAuthStore((state) => state.isLogin);
-
-  useEffect(() => {
-    if (!isLogin) {
-      navigate("/login");
-    }
-  }, [isLogin, navigate]);
-
   return (
     <>
       <UserInfo />
