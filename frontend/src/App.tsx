69549fdf0b86fb98abfba2a9e3a45f5e084fd1cc
김봉상 <tph02361@gmail.com>
2024-11-04 18:00:56 +0900

    [FE] Routing 경로 알맞게 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,21 +17,10 @@ import SituationPractice from "./pages/SituationPractice/SituationPractice";
 import AnnouncerPractice from "./pages/AnnouncerImmitate/AnnouncerPractice";
 import GameStage from "./pages/Game/GameStage";
 import Game from "./pages/Game/Game";
-import Analysis from "./pages/Analysis/Analysis";
+import MyVoice from "./pages/Analysis/Analysis";
 
 function AppWrapper() {
-  const hiddenHeaderRoutes = ["/login", "/user/regist/information"];
-  // useEffect(() => {
-  //   const accessToken = sessionStorage.getItem("accessToken");
-
-  //   // 인증이 필요 없는 경로 리스트
-  //   const publicPaths = ["/user/login", "/user/signUp", "/introduce"];
-
-  //   // 현재 경로가 인증이 필요 없는 경로가 아닌데, 토큰이 없으면 로그인 페이지로 이동
-  //   if (!accessToken && !publicPaths.includes(location.pathname)) {
-  //     navigate("/introduce");
-  //   }
-  // }, [location, navigate]);
+  const hiddenHeaderRoutes = ["/user/regist/information"];
   return (
     <>
       {!hiddenHeaderRoutes.includes(location.pathname) && <Header />}
@@ -40,23 +29,15 @@ function AppWrapper() {
         <Route element={<PublicRoute />}>
           <Route path="/login" element={<Login />} />
         </Route>
-        <Route path="/user/regist/information" element={<AccessPage />} />
 
         {/* Protected Routes - 비로그인 사용자는 /login으로 리다이렉트 */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/voice/analysis" element={<VoiceAnalysis />} />
-          <Route path="/testpage" element={<TestPage />} />
           <Route path="/" element={<MainPage />} />
-          <Route
-            path="/pronounce/practice/select"
-            element={<PracticeSelect />}
-          />
+          <Route path="/voice/analysis" element={<VoiceAnalysis />} />
+          <Route path="/pronounce" element={<PracticeSelect />} />
           <Route path="/pronounce/:options" element={<PracticePronouce />} />
-          <Route path="/pronounce/lecture" element={<Lecture />} />
-          <Route
-            path="/situation/practice/select"
-            element={<SelectOptions />}
-          />
+          <Route path="/lecture" element={<Lecture />} />
+          <Route path="/situation" element={<SelectOptions />} />
           <Route
             path="/situation/practice/regist/script"
             element={<RegistScript />}
@@ -66,11 +47,14 @@ function AppWrapper() {
             element={<SelectScript />}
           />
           <Route path="/situation/practice/" element={<SituationPractice />} />
-          <Route path="/announcer/imitate" element={<AnnouncerPractice />} />
+          <Route path="/announcer" element={<AnnouncerPractice />} />
           <Route path="/game/stage" element={<GameStage />} />
           <Route path="/game" element={<Game />} />
-          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/myvoice" element={<MyVoice />} />
         </Route>
+
+        <Route path="/user/regist/information" element={<AccessPage />} />
+        <Route path="/testpage" element={<TestPage />} />
       </Routes>
     </>
   );
