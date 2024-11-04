c7e1f4038c29f77ce83539184de68d2a7e560068
이준형 <wns0865@naver.com>
2024-11-04 16:29:40 +0900

    [BE] fix: 음성 분석 결과 반환 수정

--- backend/spring-server/src/main/java/com/fixspeech/spring_server/domain/record/repository/UserVoiceRepository.java
+++ backend/spring-server/src/main/java/com/fixspeech/spring_server/domain/record/repository/UserVoiceRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fixspeech.spring_server.domain.record.model.UserVoiceFile;
 
 public interface UserVoiceRepository extends JpaRepository<UserVoiceFile,Long> {
+	UserVoiceFile findTopByUserIdOrderByCreatedAtDesc(Long userId);
 }
