3272a621009c48fee5f268ab7f5c0acfb0884b9b
김봉상 <tph02361@gmail.com>
2024-11-04 15:38:49 +0900

    [FE] feat: createBrowserRouter로 라우팅 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Login from "./pages/user/Login";
 import MainPage from "./pages/MainPage/MainPage";
@@ -19,57 +24,123 @@ import GameStage from "./pages/Game/GameStage";
 import Game from "./pages/Game/Game";
 import Analysis from "./pages/Analysis/Analysis";
 
-function AppWrapper() {
+// 레이아웃 컴포넌트
+const Layout = () => {
   const hiddenHeaderRoutes = ["/login", "/user/regist/information"];
-  // useEffect(() => {
-  //   const accessToken = sessionStorage.getItem("accessToken");
+  const location = useLocation();
 
-  //   // 인증이 필요 없는 경로 리스트
-  //   const publicPaths = ["/user/login", "/user/signUp", "/introduce"];
-
-  //   // 현재 경로가 인증이 필요 없는 경로가 아닌데, 토큰이 없으면 로그인 페이지로 이동
-  //   if (!accessToken && !publicPaths.includes(location.pathname)) {
-  //     navigate("/introduce");
-  //   }
-  // }, [location, navigate]);
   return (
     <>
       {!hiddenHeaderRoutes.includes(location.pathname) && <Header />}
-      <Routes>
-        {/* Public Routes - 로그인 사용자는 / 으로 리다이렉트 */}
-        <Route element={<PublicRoute />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-        <Route path="/user/regist/information" element={<AccessPage />} />
-
-        {/* Protected Routes - 비로그인 사용자는 /login으로 리다이렉트 */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/voice/analysis" element={<VoiceAnalysis />} />
-          <Route path="/testpage" element={<TestPage />} />
-          <Route path="/" element={<MainPage />} />
-          <Route path="/pronounce/practice/select" element={<PracticeSelect />} />
-          <Route path="/pronounce/practice" element={<PracticePronouce />} />
-          <Route path='/pronounce/lecture' element={<Lecture/>}/>
-          <Route path='/situation/practice/select' element={<SelectOptions/>}/>
-          <Route path='/situation/practice/select/script' element={<RegistScript/>} />
-          <Route path='/situation/practice/select/script' element={<SelectScript/>} />
-          <Route path='/situation/practice/' element={<SituationPractice/>} />
-          <Route path='/announcer/imitate' element={<AnnouncerPractice/>} />
-          <Route path='/game/stage' element={<GameStage/>} />
-          <Route path='/game' element={<Game/>} />
-          <Route path='/analysis' element={<Analysis/>} />
-        </Route>
-      </Routes>
+      <Outlet />
     </>
   );
-}
+};
+
+// 라우터 설정
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      // Public Routes
+      {
+        element: <PublicRoute />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+        ],
+      },
+      // Protected Routes
+      {
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: "/",
+            element: <MainPage />,
+          },
+          {
+            path: "voice/analysis",
+            element: <VoiceAnalysis />,
+          },
+          {
+            path: "pronounce",
+            children: [
+              {
+                path: "practice/select",
+                element: <PracticeSelect />,
+              },
+              {
+                path: "practice",
+                element: <PracticePronouce />,
+              },
+              {
+                path: "lecture",
+                element: <Lecture />,
+              },
+            ],
+          },
+          {
+            path: "situation/practice",
+            children: [
+              {
+                path: "select",
+                element: <SelectOptions />,
+              },
+              {
+                path: "select/script",
+                element: <RegistScript />,
+              },
+              {
+                path: "select/script",
+                element: <SelectScript />,
+              },
+              {
+                path: "",
+                element: <SituationPractice />,
+              },
+            ],
+          },
+          {
+            path: "announcer/imitate",
+            element: <AnnouncerPractice />,
+          },
+          {
+            path: "game",
+            children: [
+              {
+                path: "stage",
+                element: <GameStage />,
+              },
+              {
+                path: "",
+                element: <Game />,
+              },
+            ],
+          },
+          {
+            path: "analysis",
+            element: <Analysis />,
+          },
+        ],
+      },
+      // Other Routes
+      {
+        path: "user/regist/information",
+        element: <AccessPage />,
+      },
+      {
+        path: "testpage",
+        element: <TestPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <Router>
-      <AppWrapper />
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
