e5f02dc937d056ce7e1544fc7c6bb11c0b5442d6
김봉상 <tph02361@gmail.com>
2024-11-05 11:13:50 +0900

    [FE] feat: 변경된 라우팅 구조에 맞게 수정, 기본 화면 일부 추가

---
+++ frontend/src/pages/PronounceTraining/TrainSelectPage.tsx
@@ -0,0 +1,88 @@
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+// 아이콘 img import
+import puzzle from "/buttons/puzzle.png";
+import music from "/buttons/music.png";
+import Typography from "@mui/material/Typography";
+
+function TrainSelect() {
+  const navigate = useNavigate();
+
+  const gridItems = [
+    {
+      color: "#FF8C82",
+      label: "문장연습",
+      imageSrc: music,
+      url: "sentence",
+      imgMargin: 50,
+    },
+    {
+      color: "#FF8C82",
+      label: "잰말놀이",
+      imageSrc: puzzle,
+      url: "tongue-twister",
+      imgMargin: 50,
+    },
+  ];
+
+  return (
+    <Box sx={{ flexGrow: 1, marginRight: "3%", marginLeft: "3%" }}>
+      <Typography
+        variant="h5"
+        className="text-white"
+        align="center"
+        style={{ wordBreak: "keep-all", whiteSpace: "normal" }}
+      >
+        *원하는 훈련 종류를 선택해주세요.
+      </Typography>
+      <Grid
+        container
+        spacing={5}
+        justifyContent="center"
+        alignItems="center"
+        className="mt-10"
+      >
+        {gridItems.map((item, index) => (
+          <Grid
+            size={{ xs: 6, sm: 4, md: 2 }}
+            key={index}
+            sx={{
+              backgroundColor: item.color,
+              padding: 2,
+              borderRadius: "3%",
+              height: 300,
+              position: "relative",
+              overflow: "hidden",
+              cursor: "pointer",
+            }}
+            onClick={() => navigate(`/training/${item.url}`)}
+          >
+            <div
+              style={{
+                color: "white",
+                fontSize: "1.5rem",
+                wordBreak: "keep-all",
+                whiteSpace: "normal",
+              }}
+            >
+              {item.label}
+            </div>
+            <img
+              src={item.imageSrc}
+              alt={item.label}
+              style={{
+                width: "100%",
+                marginTop: item.imgMargin,
+                marginLeft: 15,
+                objectFit: "cover",
+              }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
+
+export default TrainSelect;
