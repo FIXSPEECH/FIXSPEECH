784b7ec4aa6a38cceca3d161a45cf74f61122333
iheeya <chdh8987@naver.com>
2024-11-10 14:41:46 +0900

    [FE] feat: 상황별 연습 상세 페이지 API 연결

--- frontend/src/pages/SituationPractice/SituationPracticePage.tsx
+++ frontend/src/pages/SituationPractice/SituationPracticePage.tsx
@@ -1,13 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Script from "../../components/SituationPractice/Script";
 import Timer from "../../components/SituationPractice/Timer";
 import Recorder from "../../components/Recorder";
 import { ScriptGet } from "../../services/SituationPractice/SituationPracticeGet";
 
+interface Data {
+  title: string;
+  content: string;
+  accent: string;
+  second: number;
+  createdAt: string;
+}
+
+
 function SituationPractice() {
   const {scriptId} = useParams();
   const Id = Number(scriptId)
+  const [data, setData] = useState<Data>();
 
   console.log('id 타입', typeof Id)
   
@@ -16,6 +26,11 @@ function SituationPractice() {
       try{
         const response = await ScriptGet(Id)
         console.log(response)
+
+        if (response.status === 'C000') {
+          setData(response.data);
+        }
+
       } catch(e) {
         console.log(e)
       }
@@ -26,11 +41,22 @@ function SituationPractice() {
 
   return (
     <div className="flex min-h-[80vh] items-center">
-    <Script />
+    {/* <Script content={data.content} />
     <div className="flex-col  w-2/5 justify-center align-middle">
-      <Timer />
+      <Timer seconds={data.seconds}/>
       <Recorder color={"#FFAB01"} barColor={"rgb(239,204,135)"} width={200} height={60} visualizeWidth="200px" modalType="regist"/>
-    </div>
+    </div> */}
+      {data ? (
+      <>
+        <Script content={data.content} />
+        <div className="flex-col w-2/5 justify-center align-middle">
+          <Timer seconds={data.second} />
+          <Recorder color={"#FFAB01"} barColor={"rgb(239,204,135)"} width={200} height={60} visualizeWidth="200px" modalType="regist" />
+        </div>
+      </>
+    ) : (
+      <div>Loading...</div> // 데이터 로딩 중에 표시될 내용
+    )}
   </div>
   );
 }
