da354da8db2b2eb348a91aae1c243edda3a9f43c
최다희 <chdh8987@naver.com>
2024-11-11 10:04:27 +0900

    [FE] fix: 대본 목록 삭제 모달 추가

--- frontend/src/pages/SituationPractice/SelectScriptPage.tsx
+++ frontend/src/pages/SituationPractice/SelectScriptPage.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import { ScriptListGet } from "../../services/SituationPractice/SituationPracticeGet";
 import  {DeleteIcon}  from "../../Icons/DeleteIcon";
 import { ScriptDelte } from "../../services/SituationPractice/SituationPracticePost";
+import Swal from 'sweetalert2';
+import '../../styles/SituationPractice/SwalStyles.css'
+
 
 function SelectScript() {
   const [scripts, setScripts] = useState<any>([]);
@@ -28,22 +31,55 @@ function SelectScript() {
   }
 
   const handleDelete = (scriptId: number) => {
-    const remove = async() => {
-      try{
-        const response = await ScriptDelte(scriptId)
-        console.log(response)
 
-        // 삭제 후 scripts 상태 업데이트 (삭제된 스크립트를 배열에서 제거)
-        setScripts((prevScripts: any) =>
-          prevScripts.filter((script: any) => script.scriptId !== scriptId)
-        );
+    Swal.fire({
+      title: "대본을 삭제하시겠습니까?",
+      showDenyButton: true,
+      confirmButtonText: "삭제",
+      denyButtonText: `취소`,
+      customClass: {
+        confirmButton: "swal2-confirm-btn", // 삭제 버튼
+        denyButton: "swal2-deny-btn",       // 취소 버튼
+      },
+      buttonsStyling: false
+    }).then(async (result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        try{
+          const response = await ScriptDelte(scriptId)
+          console.log(response)
+  
+          // 삭제 후 scripts 상태 업데이트 (삭제된 스크립트를 배열에서 제거)
+          setScripts((prevScripts: any) =>
+            prevScripts.filter((script: any) => script.scriptId !== scriptId)
+          );
+  
+        } catch(e) {
+          console.log(e)
+        }
 
-      } catch(e) {
-        console.log(e)
+        
+      } else if (result.isDenied) {
+        
       }
-    }
+    });
 
-    remove()
+    // const remove = async() => {
+    //   try{
+    //     const response = await ScriptDelte(scriptId)
+    //     console.log(response)
+
+    //     // 삭제 후 scripts 상태 업데이트 (삭제된 스크립트를 배열에서 제거)
+    //     setScripts((prevScripts: any) =>
+    //       prevScripts.filter((script: any) => script.scriptId !== scriptId)
+    //     );
+
+    //   } catch(e) {
+    //     console.log(e)
+    //   }
+    // }
+
+    // remove()
   }
   
 
@@ -62,7 +98,7 @@ function SelectScript() {
             <div className="flex justify-between items-center">
               <div className="text-white text-xl">{script.title}</div>
               <div className="flex items-center">
-                <div className="text-[#FFAB01] text-sm mr-2">등록일: {script.cratedAt}</div>
+                <div className="text-[#FFAB01] text-sm mr-2">등록일: {script.createdAt}</div>
                 <DeleteIcon onClick={() => handleDelete(script.scriptId)}/>
               </div>
             </div>
