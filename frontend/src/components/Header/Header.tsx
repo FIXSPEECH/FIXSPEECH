772abf5b4d7495c15aa960272314b2f344cc2a56
최다희 <chdh8987@naver.com>
2024-11-11 11:12:30 +0900

    [FE] fix: 헤더의 페이지 정보 클릭하면 해당 메인페이지로 이동하도록 수정

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -10,23 +10,23 @@ function Header() {
     const path = location.pathname.split("/")[1]; // 경로의 첫 번째 부분 (예: /situation/select => 'situation')
 
     if (path === "situation") {
-      return { title: "상황별 연습", colorClass: "text-[#FFAB01]" };
+      return { title: "상황별 연습", colorClass: "text-[#FFAB01]", url: '/situation' };
     } else if (path === "training") {
-      return { title: "발음 훈련 연습", colorClass: "text-[#FF8C82]" };
+      return { title: "발음 훈련 연습", colorClass: "text-[#FF8C82]", url: '/training' };
     } else if (path == "lecture") {
-      return { title: "발음 훈련 강의", colorClass: "text-[#EE719E]" };
+      return { title: "발음 훈련 강의", colorClass: "text-[#EE719E]", url: '/lecture' };
     } else if (path === "announcer") {
-      return { title: "아나운서 따라잡기", colorClass: "text-[#B18CFE]" };
+      return { title: "아나운서 따라잡기", colorClass: "text-[#B18CFE]", url: '/announcer' };
     } else if (path === "game") {
-      return { title: "산성비 게임", colorClass: "text-[#FE6250]" };
+      return { title: "산성비 게임", colorClass: "text-[#FE6250]", url: '/game' };
     } else if (path === "analysis") {
-      return { title: "내 목소리 분석", colorClass: "text-[#37AFE1]" };
+      return { title: "내 목소리 분석", colorClass: "text-[#37AFE1]", url:'/analysis' };
     }
 
-    return { title: "", colorClass: "" };
+    return { title: "", colorClass: "", url: '/' };
   };
 
-  const { title, colorClass } = getHeaderTitle();
+  const { title, colorClass, url } = getHeaderTitle();
 
   return (
     <div className="logo-header font-bold flex justify-between items-center">
@@ -39,7 +39,9 @@ function Header() {
       </span>
 
       {/* 경로에 따라 제목 표시 */}
-      <span className={colorClass}>{title}</span>
+      <span className={colorClass}
+        style={{ cursor: "pointer" }}
+        onClick={() => navigate(url)}>{title}</span>
     </div>
   );
 }
