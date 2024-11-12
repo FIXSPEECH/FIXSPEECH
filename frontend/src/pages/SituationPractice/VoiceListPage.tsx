366f149634e7faf6fdb17662151f68e5e4489164
최다희 <chdh8987@naver.com>
2024-11-12 10:25:46 +0900

    [FE] feat: 상황별연습 녹음 리스트 api 연결

--- frontend/src/pages/SituationPractice/VoiceListPage.tsx
+++ frontend/src/pages/SituationPractice/VoiceListPage.tsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { ScriptListGet } from "../../services/SituationPractice/SituationPracticeGet";
-import  {DeleteIcon}  from "../../Icons/DeleteIcon";
-import { ScriptDelte } from "../../services/SituationPractice/SituationPracticePost";
-import Swal from 'sweetalert2';
+import { useNavigate, useParams } from "react-router-dom";
+import { VoiceListGet } from "../../services/SituationPractice/SituationPracticeGet";
+// import  {DeleteIcon}  from "../../Icons/DeleteIcon";
+// import { ScriptDelte } from "../../services/SituationPractice/SituationPracticePost";
+// import Swal from 'sweetalert2';
 import '../../styles/SituationPractice/SwalStyles.css'
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
 
-function SelectResult() {
+function VoiceList() {
   const [scripts, setScripts] = useState<any>([]);
   const [currentPage, setCurrentPage] = useState<number>(1); // 현재 페이지
   const [scriptsPerPage] = useState<number>(5); // 페이지당 스크립트 수
   const navigate = useNavigate();
+  const {scriptId} = useParams()
+  const Id = Number(scriptId)
 
   useEffect(() => {
-    const getScript = async() => {
+    const VoiceList = async() => {
       try{
-        const response = await ScriptListGet();
+        const response = await VoiceListGet(0, 10, Id);
         console.log(response)
         setScripts(response)
       } catch (e) {
@@ -26,7 +28,7 @@ function SelectResult() {
       }
     }
 
-    getScript();
+    VoiceList();
 
   }, [])
 
@@ -46,41 +48,41 @@ function SelectResult() {
     navigate(`/situation/voice/${scriptId}`)
   }
 
-  const handleDelete = (scriptId: number) => {
+//   const handleDelete = (scriptId: number) => {
 
-    Swal.fire({
-      title: "대본을 삭제하시겠습니까?",
-      showDenyButton: true,
-      confirmButtonText: "삭제",
-      denyButtonText: `취소`,
-      customClass: {
-        confirmButton: "swal2-confirm-btn", // 삭제 버튼
-        denyButton: "swal2-deny-btn",       // 취소 버튼
-      },
-      buttonsStyling: false
-    }).then(async (result) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (result.isConfirmed) {
-        try{
-          const response = await ScriptDelte(scriptId)
-          console.log(response)
+//     Swal.fire({
+//       title: "대본을 삭제하시겠습니까?",
+//       showDenyButton: true,
+//       confirmButtonText: "삭제",
+//       denyButtonText: `취소`,
+//       customClass: {
+//         confirmButton: "swal2-confirm-btn", // 삭제 버튼
+//         denyButton: "swal2-deny-btn",       // 취소 버튼
+//       },
+//       buttonsStyling: false
+//     }).then(async (result) => {
+//       /* Read more about isConfirmed, isDenied below */
+//       if (result.isConfirmed) {
+//         try{
+//           const response = await ScriptDelte(scriptId)
+//           console.log(response)
   
-          // 삭제 후 scripts 상태 업데이트 (삭제된 스크립트를 배열에서 제거)
-          setScripts((prevScripts: any) =>
-            prevScripts.filter((script: any) => script.scriptId !== scriptId)
-          );
+//           // 삭제 후 scripts 상태 업데이트 (삭제된 스크립트를 배열에서 제거)
+//           setScripts((prevScripts: any) =>
+//             prevScripts.filter((script: any) => script.scriptId !== scriptId)
+//           );
   
-        } catch(e) {
-          console.log(e)
-        }
+//         } catch(e) {
+//           console.log(e)
+//         }
 
         
-      } else if (result.isDenied) {
+//       } else if (result.isDenied) {
         
-      }
-    });
+//       }
+//     });
 
-  }
+//   }
 
   
 
@@ -90,7 +92,7 @@ function SelectResult() {
         음성 녹음 목록
       </div>
       {scripts.length === 0 ? (
-        <div className="text-[#FFAB01] text-lg">저장된 대본이 없습니다</div>
+        <div className="text-[#FFAB01] text-lg">저장된 음성녹음 없습니다</div>
       ) : (
         <>
           <div className="space-y-3">
@@ -101,10 +103,11 @@ function SelectResult() {
                 onClick={() => handleClick(script.scriptId)}
               >
                 <div className="flex justify-between items-center">
-                  <div className="text-white text-xl">{script.title}</div>
+                  <div className="text-white text-xl">{script.score}점</div>
+                  <div className="text-[#FFAB01] text-lg">분석결과 확인하기</div>
                   <div className="flex items-center">
                     <div className="text-[#FFAB01] text-sm mr-2">등록일: {script.createdAt}</div>
-                    <DeleteIcon onClick={() => handleDelete(script.scriptId)} />
+                    {/* <DeleteIcon onClick={() => handleDelete(script.scriptId)} /> */}
                   </div>
                 </div>
               </div>
@@ -142,4 +145,4 @@ function SelectResult() {
   );
 }
 
-export default SelectResult;
+export default VoiceList;
--- frontend/src/services/SituationPractice/SituationPracticeGet.tsx
+++ frontend/src/services/SituationPractice/SituationPracticeGet.tsx
@@ -8,6 +8,12 @@ interface Scripts {
     createdAt: string;
 }
 
+interface VoiceList {
+    resultId: number;
+    score: number;
+    createdAt: string;
+}
+
 export async function ScriptListGet(page = 0, size = 10) {
     try {
         let results: Scripts[] = [];
@@ -43,4 +49,30 @@ export async function ScriptGet (scriptId: number) {
         .catch((error) => {
             return Promise.reject(error)
         })
+}
+
+
+
+export async function VoiceListGet(page = 0, size = 10, scriptId: number) {
+    try {
+        let results: VoiceList[] = [];
+        let hasMoreData = true;
+
+        while (hasMoreData) {
+            const response = await axiosInstance.get(`/script/result/${scriptId}?page=${page}&size=${size}`);
+            const data =  response.data.data.content;
+
+            results = [...results, ...data];
+
+            if (data.length < size) {
+                hasMoreData = false;  // 더 이상 데이터가 없다고 판단하여 반복 종료
+            } else {
+                page += 1;  // 다음 페이지 요청
+            }
+        }
+
+        return Promise.resolve(results);
+    } catch (error) {
+        return Promise.reject(error);
+    }
 }
