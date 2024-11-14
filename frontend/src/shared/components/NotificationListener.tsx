564709daa15db87d35b5c4d854c47d105bf9b38f
김도환 <rlaehghkr@gmail.com>
2024-11-14 09:59:46 +0900

    [FE] fix: EventSource 경로 수정

--- frontend/src/shared/components/NotificationListener.tsx
+++ frontend/src/shared/components/NotificationListener.tsx
@@ -5,14 +5,16 @@ import 'react-toastify/dist/ReactToastify.css';
 const NotificationListener = () => {
   useEffect(() => {
     // SSE 구독 시작
-    const eventSource = new EventSource('/notifications/subscribe');
+    const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/notifications/subscribe`);
 
     // "analysisComplete" 이벤트 수신
     eventSource.addEventListener('analysisComplete', (event) => {
       const data = JSON.parse(event.data);
       if (data.type === 'Analyze Complete') {
+        console.log("알림 받음")
         toast.success(`알림: ${data.message}`);
       } else if (data.type === 'ANALYSIS_ERROR') {
+        console.log("SSE 에러 발생")
         toast.error(`에러: ${data.message}`);
       }
     });
