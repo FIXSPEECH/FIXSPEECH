5c716cf5da5654438b63842f45272319dd4c9d2e
iheeya <chdh8987@naver.com>
2024-11-14 23:37:30 +0900

    [FE] feat: 음성 단일 분석결과 삭제 api 연결

--- frontend/src/pages/SituationPractice/VoiceListPage.tsx
+++ frontend/src/pages/SituationPractice/VoiceListPage.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
 import { VoiceListGet } from "../../services/SituationPractice/SituationPracticeGet";
-// import  {DeleteIcon}  from "../../Icons/DeleteIcon";
-// import { ScriptDelte } from "../../services/SituationPractice/SituationPracticePost";
-// import Swal from 'sweetalert2';
+import { DeleteIcon } from "../../shared/components/Icons/DeleteIcon";
+import { ScriptVoiceResultDelete } from "../../services/SituationPractice/SituationPracticePost";
+import Swal from 'sweetalert2';
 import "./SwalStyles.css";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
@@ -47,40 +47,40 @@ function VoiceList() {
     navigate(`/situation/voice/${scriptId}`);
   };
 
-  //   const handleDelete = (scriptId: number) => {
+    const handleDelete = (resultId: number) => {
 
-  //     Swal.fire({
-  //       title: "대본을 삭제하시겠습니까?",
-  //       showDenyButton: true,
-  //       confirmButtonText: "삭제",
-  //       denyButtonText: `취소`,
-  //       customClass: {
-  //         confirmButton: "swal2-confirm-btn", // 삭제 버튼
-  //         denyButton: "swal2-deny-btn",       // 취소 버튼
-  //       },
-  //       buttonsStyling: false
-  //     }).then(async (result) => {
-  //       /* Read more about isConfirmed, isDenied below */
-  //       if (result.isConfirmed) {
-  //         try{
-  //           const response = await ScriptDelte(scriptId)
-  //           console.log(response)
+      Swal.fire({
+        title: "분석결과를 삭제하시겠습니까?",
+        showDenyButton: true,
+        confirmButtonText: "삭제",
+        denyButtonText: `취소`,
+        customClass: {
+          confirmButton: "swal2-confirm-btn", // 삭제 버튼
+          denyButton: "swal2-deny-btn",       // 취소 버튼
+        },
+        buttonsStyling: false
+      }).then(async (result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          try{
+            const response = await ScriptVoiceResultDelete(resultId)
+            console.log(response)
 
-  //           // 삭제 후 scripts 상태 업데이트 (삭제된 스크립트를 배열에서 제거)
-  //           setScripts((prevScripts: any) =>
-  //             prevScripts.filter((script: any) => script.scriptId !== scriptId)
-  //           );
+            // 삭제 후 scripts 상태 업데이트 (삭제된 스크립트를 배열에서 제거)
+            setScripts((prevScripts: any) =>
+              prevScripts.filter((script: any) => script.scriptId !== scriptId)
+            );
 
-  //         } catch(e) {
-  //           console.log(e)
-  //         }
+          } catch(e) {
+            console.log(e)
+          }
 
-  //       } else if (result.isDenied) {
+        } else if (result.isDenied) {
 
-  //       }
-  //     });
+        }
+      });
 
-  //   }
+    }
 
   return (
     <div className="h-screen p-8">
@@ -107,7 +107,8 @@ function VoiceList() {
                     <div className="text-[#FFAB01] text-sm mr-2">
                       등록일: {script.createdAt}
                     </div>
-                    {/* <DeleteIcon onClick={() => handleDelete(script.scriptId)} /> */}
+
+                    <DeleteIcon onClick={() => handleDelete(script.resultId)} />
                   </div>
                 </div>
               </div>
