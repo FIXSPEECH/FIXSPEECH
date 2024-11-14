a108bc4cf978f7286733a20f16e7bbe8127ab941
김도환 <rlaehghkr@gmail.com>
2024-11-14 12:46:50 +0900

    [FE] feat: 알람 기능 구현 (header 추가)

--- frontend/src/shared/components/NotificationListener.tsx
+++ frontend/src/shared/components/NotificationListener.tsx
@@ -1,20 +1,30 @@
-import { useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { EventSourcePolyfill } from "event-source-polyfill"; // Polyfill import
+import useAuthStore from "../stores/authStore";
 
 const NotificationListener = () => {
   useEffect(() => {
-    // SSE 구독 시작
-    const eventSource = new EventSource(`${import.meta.env.VITE_API_URL}/notifications/subscribe`);
+    // 토큰 가져오기
+    const token = useAuthStore.getState().token;
+
+    // SSE 구독 시작 - Authorization 헤더 포함
+    const eventSource = new EventSourcePolyfill(`${import.meta.env.VITE_API_URL}/notifications/subscribe`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      },
+    });
 
     // "analysisComplete" 이벤트 수신
-    eventSource.addEventListener('analysisComplete', (event) => {
+    eventSource.addEventListener("analysisComplete", (event: any) => {
       const data = JSON.parse(event.data);
-      if (data.type === 'Analyze Complete') {
-        console.log("알림 받음")
-        toast.success(`알림: ${data.message}`);
-      } else if (data.type === 'ANALYSIS_ERROR') {
-        console.log("SSE 에러 발생")
+      if (data.type === "Analyze Complete") {
+        toast.success(`${data.message}`);
+      } else if (data.type === "ANALYSIS_ERROR") {
         toast.error(`에러: ${data.message}`);
       }
     });
