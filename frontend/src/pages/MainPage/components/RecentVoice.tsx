f2abef574dfae71e1a3df2b875df8a4799e686e9
김봉상 <tph02361@gmail.com>
2024-11-14 14:41:00 +0900

    [FE] feat: 목소리 녹음 UX 수정

--- frontend/src/pages/MainPage/components/RecentVoice.tsx
+++ frontend/src/pages/MainPage/components/RecentVoice.tsx
@@ -215,7 +215,7 @@ function RecentVoice() {
                     drag="x"
                     dragConstraints={{ left: 0, right: 0 }}
                     dragElastic={1}
-                    onDragEnd={(e, { offset, velocity }) => {
+                    onDragEnd={(_e, { offset, velocity }) => {
                       const swipe = swipePower(offset.x, velocity.x);
                       if (swipe < -swipeConfidenceThreshold) {
                         paginate(1);
