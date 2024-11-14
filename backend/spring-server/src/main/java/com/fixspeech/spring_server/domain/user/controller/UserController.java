3442510b8a72b7d155eebce212a9386817bb4785
김찬민 <thesimpleman@naver.com>
2024-11-14 09:04:45 +0900

    [BE] fix: 토큰 재발급 기능 임시 수정

--- backend/spring-server/src/main/java/com/fixspeech/spring_server/domain/user/controller/UserController.java
+++ backend/spring-server/src/main/java/com/fixspeech/spring_server/domain/user/controller/UserController.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,13 +60,47 @@ public class UserController {
 		}
 	}
 
+	// /**
+	//  * Token 재발급
+	//  * @param refreshToken
+	//  * @return
+	//  */
+	// @PostMapping("/public/reissue")
+	// public ApiResponse<?> reissueToken(HttpServletRequest httpServletRequest, @CookieValue("refresh-token") String refreshToken,
+	// 	HttpServletResponse response) {
+	//
+	// 	log.info("refreshToken = {}", refreshToken);
+	// 	try {
+	// 		if (refreshToken == null || refreshToken.isEmpty()) {
+	// 			return ApiResponse.createError(ErrorCode.INVALID_TOKEN_ERROR);
+	// 		}
+	//
+	// 		ResponseRefreshTokenDTO responseDTO = tokenService.reissueOAuthToken(refreshToken);
+	// 		log.info("responseDTO={}", responseDTO);
+	// 		if (responseDTO == null) {
+	// 			throw new IllegalArgumentException("Refresh Token이 만료되었거나 존재하지 않습니다.");
+	// 		}
+	// 		log.info("new AccessToken = {}", responseDTO.getAccessToken());
+	// 		String newAccessToken = responseDTO.getAccessToken();
+	// 		String newRefreshToken = responseDTO.getRefreshToken();
+	//
+	// 		ResponseCookie responseCookie = jwtCookieProvider.generateCookie(newRefreshToken);
+	//
+	// 		response.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + newAccessToken);
+	// 		response.setHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
+	// 		return ApiResponse.createSuccess(newAccessToken, "토큰 재발급 성공");
+	// 	} catch (Exception e) {
+	// 		log.info("e={}",e);
+	// 		return ApiResponse.createError(ErrorCode.INVALID_JWT_TOKEN);
+	// 	}
+	// }
 	/**
 	 * Token 재발급
 	 * @param refreshToken
 	 * @return
 	 */
 	@PostMapping("/public/reissue")
-	public ApiResponse<?> reissueToken(HttpServletRequest httpServletRequest, @CookieValue("refresh-token") String refreshToken,
+	public ApiResponse<?> reissueToken(HttpServletRequest httpServletRequest, @RequestBody String refreshToken,
 		HttpServletResponse response) {
 
 		log.info("refreshToken = {}", refreshToken);
