396d35c5818e17357e312b321412f4e53b826a73
김찬민 <thesimpleman@naver.com>
2024-11-15 09:50:05 +0900

    [BE] fix: jwt 토큰 반환시 name 제외 추가

--- backend/spring-server/src/main/java/com/fixspeech/spring_server/domain/user/model/JwtUserClaims.java
+++ backend/spring-server/src/main/java/com/fixspeech/spring_server/domain/user/model/JwtUserClaims.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class JwtUserClaims {
-	private String name; // Email
+	private String name;
 	private String nickName;
 	private String email;
 	private String gender;
