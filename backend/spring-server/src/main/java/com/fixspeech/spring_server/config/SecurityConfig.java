1d350d6f283af3192e01070da1e18f4de89f17ea
김찬민 <thesimpleman@naver.com>
2024-11-18 17:00:16 +0900

    [BE] fix: requestMatchers 수정

--- backend/spring-server/src/main/java/com/fixspeech/spring_server/config/SecurityConfig.java
+++ backend/spring-server/src/main/java/com/fixspeech/spring_server/config/SecurityConfig.java
@@ -55,8 +55,10 @@ public class SecurityConfig {
 			.csrf(AbstractHttpConfigurer::disable)
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
 			.authorizeHttpRequests(requests -> requests
-				.requestMatchers("/user/logout", "/login", "/api/login", "/login/kakao", "/oauth/login/**", "/api/oauth/login/**", "/user/public/reissue")
-//				.requestMatchers("*/*", "*", "**", "**/**")r
+				.requestMatchers("/user/logout", "/login", "/api/login", "/login/kakao",
+					"/oauth/login/**", "/api/oauth/login/**", "/user/public/reissue",
+					"/swagger-ui/**", "/v3/api-docs/**")
+//				.requestMatchers("*/*", "*", "**", "**/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
