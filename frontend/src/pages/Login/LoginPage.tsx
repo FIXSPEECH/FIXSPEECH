c226488019de554c0b62afa3e6c3a0ac310a978a
최다희 <chdh8987@naver.com>
2024-11-18 14:32:21 +0900

    [FE] fix: 테스트 로그인 버튼 함수 주석 처리

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,27 +1,27 @@
 import "./Login.css";
-import useAuthStore from "../../shared/stores/authStore";
-import { useNavigate } from "react-router-dom";
+// import useAuthStore from "../../shared/stores/authStore";
+// import { useNavigate } from "react-router-dom";
 import AudioVertexVisualizer from "../../shared/components/Visualizer/AudioVertexVisualizer";
 
 function Login() {
-  const setToken = useAuthStore((state) => state.setToken);
-  const navigate = useNavigate();
+  // const setToken = useAuthStore((state) => state.setToken);
+  // const navigate = useNavigate();
 
   const handleLogin = () => {
     window.location.href =
       import.meta.env.VITE_API_URL + "/oauth2/authorization/kakao";
   };
 
-  // 테스트 로그인 처리
-  const handleTestLogin = (gender: "male" | "female") => {
-    const testToken =
-      gender === "male"
-        ? import.meta.env.VITE_TEST_TOKEN_MALE
-        : import.meta.env.VITE_TEST_TOKEN_FEMALE;
+  // // 테스트 로그인 처리
+  // const handleTestLogin = (gender: "male" | "female") => {
+  //   const testToken =
+  //     gender === "male"
+  //       ? import.meta.env.VITE_TEST_TOKEN_MALE
+  //       : import.meta.env.VITE_TEST_TOKEN_FEMALE;
 
-    setToken(testToken);
-    navigate("/");
-  };
+  //   setToken(testToken);
+  //   navigate("/");
+  // };
 
   return (
     <main className="flex flex-col items-center justify-center">
